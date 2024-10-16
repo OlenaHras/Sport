2959e9a81ea719c612c19c4574234f4e4e5feac7
OlenaHras <alena.hras@gmail.com>
2024-10-16 15:15:51 +0300

    add modal windows, localstore for saving users

--- js/main.js
+++ js/main.js
@@ -32,6 +32,128 @@ faqButtons.forEach((faqButton, i) => {
       faqButton.classList.add("open");
       faqAnswers[i].classList.add("open");
     }
-    // faqButton.nextElementSibling.classList.toggle("open");
   });
+});
+
+// Modal
+
+const modalCloseBtn = document.querySelector(".modal-close-btn");
+const singupModalCloseBtn = document.querySelector(".signup-modal-close-btn");
+const openLoginModalBtn = document.querySelector("#login-button");
+const openSignupModalBtn = document.querySelector("#signup-button");
+const loginModal = document.querySelector(".login-modal");
+const signupModal = document.querySelector(".signup-modal");
+const overlay = document.querySelector(".overlay");
+
+const openModal = (modal) => {
+  if (modal === "login") {
+    loginModal.classList.remove("hidden");
+  } else if (modal === "signup") {
+    signupModal.classList.remove("hidden");
+  }
+  overlay.classList.remove("hidden");
+};
+
+const closeModal = (modal) => {
+  if (modal === "login") {
+    loginModal.classList.add("hidden");
+  } else if (modal === "signup") {
+    signupModal.classList.add("hidden");
+  } else {
+    loginModal.classList.add("hidden");
+    signupModal.classList.add("hidden");
+  }
+  overlay.classList.add("hidden");
+};
+
+openLoginModalBtn.addEventListener("click", () => openModal("login"));
+openSignupModalBtn.addEventListener("click", () => openModal("signup"));
+modalCloseBtn.addEventListener("click", () => closeModal("login"));
+singupModalCloseBtn.addEventListener("click", () => closeModal("signup"));
+overlay.addEventListener("click", closeModal);
+
+// LogIn SingUp Locallstorage
+
+const loginForm = document.querySelector("#login-form");
+const signupForm = document.querySelector("#signup-form");
+
+signupForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const name = document.querySelector("#new-name").value;
+  const email = document.querySelector("#new-email").value;
+  const phone = document.querySelector("#new-phone").value;
+  const password = document.querySelector("#new-password").value;
+
+  const newUser = {
+    name,
+    email,
+    phone,
+    password,
+  };
+
+  const users = JSON.parse(localStorage.getItem("users")) || [];
+
+  const foundUser = users.find(
+    (user) => user.email === email && user.password === password
+  );
+  if (!foundUser) {
+    users.push(newUser);
+    Toastify({
+      text: `User ${name} created successfully`,
+      duration: 3000,
+      gravity: "top",
+      position: "right",
+      backgroundColor: "linear-gradient(to right, #00b09b, #96c93d)",
+    }).showToast();
+  } else {
+    Toastify({
+      text: `User with email ${email} already exists`,
+      duration: 3000,
+      gravity: "top",
+      position: "right",
+      backgroundColor: "linear-gradient(to right, #00b09b, #96c93d)",
+    }).showToast();
+  }
+
+  localStorage.setItem("users", JSON.stringify(users));
+
+  closeModal("signup");
+});
+
+loginForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const email = document.querySelector("#email").value;
+  const password = document.querySelector("#password").value;
+
+  const users = JSON.parse(localStorage.getItem("users")) || [];
+
+  const foundUser = users.find(
+    (user) => user.email === email && user.password === password
+  );
+
+  if (!foundUser) {
+    Toastify({
+      text: "Incorrect email or password",
+      duration: 3000,
+
+      newWindow: true,
+      close: true,
+      gravity: "top",
+      position: "right",
+      stopOnFocus: true,
+      style: {
+        background: "linear-gradient(to right, #00b09b, #96c93d)",
+      },
+    }).showToast();
+  } else {
+    Toastify({
+      text: `Welcome ${foundUser.name} !`,
+      duration: 3000,
+      gravity: "top",
+      position: "right",
+      backgroundColor: "linear-gradient(to right, #00b09b, #96c93d)",
+    }).showToast();
+  }
+
+  closeModal("login");
 });
